8bb93374231f847c4986a812200c7fa96631704f
Jonathan Onrubia Solis <jonathan.onrubia@vass.es>
2023-12-06 16:15:22 +0100

    Project init

---
+++ FilmFlow/FilmFlow/FilmFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFlowApp.swift
+//  FilmFlow
+//
+//  Created by Jonathan Onrubia Solis on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FilmFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
